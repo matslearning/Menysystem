d33322aefce1338f4b6e3edbd4e06337784f99e1
Mats <matslearning@outlook.com>
2020-08-27 10:34:56 +0200

    Add project files.

---
+++ Menysystem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Menysystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkomna till huvudmenyn!");
+            Console.WriteLine("");
+        }
+    }
+}
